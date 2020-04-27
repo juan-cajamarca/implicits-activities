28f0450be4e892b6ce244c2818dd6fe56a1ce89e
Juan Cajamarca <jcajamarca@nativapps.co>
2020-04-26 23:10:20 -0500

    App done

--- app/src/main/java/com/example/implicitactivities/MainActivity.java
+++ app/src/main/java/com/example/implicitactivities/MainActivity.java
@@ -1,32 +1,69 @@
 package com.example.implicitactivities;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.provider.ContactsContract;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText contactName;
+    EditText phoneNumber;
+    Spinner phoneTypeSpinner;
+    EditText contactEmail;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+
+        // Getting view elements
+        contactName = (EditText) findViewById(R.id.contact_name_edit_text);
+        phoneNumber = (EditText) findViewById(R.id.contact_phone_edit_text);
+        phoneTypeSpinner = (Spinner) findViewById(R.id.phone_type_spinner);
+        contactEmail = (EditText) findViewById(R.id.contact_email_text_view);
+
+        // Adding spinner options
+        List spinnerList = new ArrayList<String>();
+        spinnerList.add("Personal");
+        spinnerList.add("Laboral");
+        spinnerList.add("Escolar");
+
+        // Setting array adapter for spinner
+        ArrayAdapter spinnerListAdapter = new ArrayAdapter(this, android.R.layout.simple_dropdown_item_1line, spinnerList);
+        spinnerListAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        phoneTypeSpinner.setAdapter(spinnerListAdapter);
 
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                // Setting intent
+                Intent intent = new Intent(ContactsContract.Intents.Insert.ACTION);
+                intent.setType(ContactsContract.RawContacts.CONTENT_TYPE);
+
+                // Passing data
+                intent.putExtra(ContactsContract.Intents.Insert.NAME, phoneNumber.getText())
+                        .putExtra(ContactsContract.Intents.Insert.PHONE, phoneNumber.getText())
+                        .putExtra(ContactsContract.Intents.Insert.PHONE_TYPE, phoneTypeSpinner.getSelectedItem().toString())
+                        .putExtra(ContactsContract.Intents.Insert.EMAIL, contactEmail.getText());
+
+                // Starting activity
+                startActivity(intent);
             }
         });
     }
